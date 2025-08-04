27168a3660859590fff166d6a2714c57c7154700
kanthimathi-vr <mathi.v.r.1990@gmail.com>
2025-08-04 23:45:37 +0530

    book

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,  include
+from core.views import create_admin_view 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
      path('', include('myapp.urls')),
+      path('create-admin/', create_admin_view), 
 
 ]

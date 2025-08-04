b9dc27206f6c4c84e53755c1046e30e771a2d401
kanthimathi-vr <mathi.v.r.1990@gmail.com>
2025-08-04 23:57:56 +0530

    book

--- myapp/views.py
+++ myapp/views.py
@@ -1,12 +1,3 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
-from django.contrib.auth.models import User
-
-def create_admin_view(request):
-    if not User.objects.filter(username='admin').exists():
-        User.objects.create_superuser('admin', 'admin@example.com', 'adminpassword')
-        return HttpResponse("Superuser created successfully.")
-    else:
-        return HttpResponse("Superuser already exists.")
